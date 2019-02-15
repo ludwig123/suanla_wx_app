8b19a22233622e0bda310bac6d1babe1a2621efb
hejing <hejingjizhen@gmail.com>
2019-02-16 00:11:05 +0800

    添加组件成功，但是事件没法响应

---
+++ src/components/item.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="search-item" >
+  	<button v-on:click="counter++">fff{{counter}}</button>
+	<div class="item-name">
+	<h3>代码：<span>{{code}}</span> 金额：<span>{{money}}</span> 计分：<span>{{score}}</span></h3>
+	</div>
+	<div class="item-content">
+	<p>内容：{{content}}</p>
+	</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['code', 'money', 'content', 'score'],
+
+  data: function () {
+    return {
+      counter: 0
+    }
+  },
+
+  methods: {
+    detail: function () {
+      console.log('dddddd')
+      this.counter += 1
+      this.$emit('increment')
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.item-name {
+    width: 75%;
+    margin-bottom: 8rpx;
+    h3 {
+        font-weight: 400;
+    }
+    span {
+        color: #0366d6;
+        display: inline-block;
+        margin-right: 16rpx;
+        &.jifen {
+            float: right;
+        }
+    }
+}
+
+.item-content {
+    color: #586069;
+    margin-top: 8rpx;
+    font-size: 22rpx;
+    span {
+        display: inline-block;
+        margin-right: 16rpx;
+        &.repo-stars-text {
+            float: right;
+        }
+    }
+}
+
+.search-item {
+    position: relative;
+    padding: 24rpx;
+    margin: 4rpx;
+    font-size: 28rpx;
+    border-bottom: 5rpx #e1e4e8 solid;
+}
+</style>
--- src/pages/codeSearch/index.vue
+++ src/pages/codeSearch/index.vue
@@ -3,10 +3,24 @@
 		<div class="show-search-bar">
 		<mp-searchbar :isFocus=true :inputValue="inputValue" :placeholder="placeholder" @input="input" @blur="blur" @focus="focus" @confirm="confirm"></mp-searchbar>
 		</div>
-		  <ul class=" jithub-list">
+
+		  <ul class="jithub-list">
+		  	<div>{{count}}</div>
+		  	<button v-on:click="count +=1">jiayi</button>
+		  	<item 
+		  	  v-on:detail="count +=1" 
+		  	  v-for="item in items" 
+		  	  v-bind:code="item['代码']"
+		  	  v-bind:money="item['金额']"
+		  	  v-bind:score="item['计分']"
+		  	  v-bind:content="item['内容']"
+		  	  >
+		  	 </item>
+
+
 		  	<li class="search-item" @click="enterRepo()">
                 <div class="item-name">
-                    <h3>代码：<span>11110</span> 金额：<span>100元</span> 计分：<span>12分</span></h3>
+                    <h3>代码：<span>11111</span> 金额：<span>100元</span> 计分：<span>12分</span></h3>
                 </div>
 		  		<div class="item-content">
 		  			<p>内容：无有效机动车驾驶证驾驶摩托车、拖拉机、营运载客汽车以外的机动车的</p>
@@ -14,7 +28,7 @@
 		  	</li>
 		  	<li class="search-item" @click = "getCodeList">
 		  		<div class="item-name">
-                    <h3>代码：<span>11110</span> 金额：<span>100元</span> 计分：<span>12分</span></h3>
+                    <h3>代码：<span>11112</span> 金额：<span>130元</span> 计分：<span>12分</span></h3>
                 </div>
 		  		<div class="item-content">
 		  			<p>内容：无有效机动车驾驶证驾驶摩托车、拖拉机、营运载客汽车以外的机动车的</p>
@@ -87,13 +101,21 @@
 <script>
 import './index.less'
 import mpSearchbar from 'mpvue-weui/src/searchbar'
+import item from '@/components/item'
+import data from '../../data/code.json'
 
 export default {
 
   data () {
     return {
-      placeholder: 'Hello World',
+      count: 0,
+      placeholder: data['0']['金额'],
       input: 'iiiii',
+      items: data,
+      code: data['0']['代码'],
+      money: data['0']['金额'],
+      score: data['0']['计分'],
+      content: data['0']['内容'],
       nodes: [{
         name: 'div',
         attrs: {
@@ -109,7 +131,8 @@ export default {
   },
 
   components: {
-    mpSearchbar
+    mpSearchbar,
+    item
   },
 
   methods: {
@@ -123,14 +146,15 @@ export default {
 
     getCodeList: function () {
       console.log('id_getCodeList')
-        this.$http.get('info.json').then(function(response){
-            console.log(response.data.data)
-            var resdata = response.data.data
-            this.peps = resdata
-        }).catch(function(response){
-            console.log(response)
-            console.log("居然没有弹窗")
-        })
+      console.log(data)
+      this.$http.get('../code.json').then(function (response) {
+        console.log(response.data.data)
+        var resdata = response.data.data
+        this.peps = resdata
+      }).catch(function (response) {
+        console.log(response)
+        console.log('居然没有弹窗')
+      })
     }
   }
 }
